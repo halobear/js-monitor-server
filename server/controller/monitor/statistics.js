17aee12c0acdf0ee7643992f740396ea1088b6f4
luzhongkuan <luzhongk@126.com>
2020-08-20 18:07:56 +0800

    feat: 监控统计

--- server/controller/monitor/statistics.js
+++ server/controller/monitor/statistics.js
@@ -1,7 +1,7 @@
 /**
  * 获取运行错误总数、获取资源错误总数、获取影响人数总数、获取影响页面总数、获取今日运行错误、获取今日资源错误、获取今日受影响人数、获取今日受影响页面
  */
-
+const moment = require('moment')
 const mysql = require('../../utils/mysql')
 const { assetsTypes } = require('../../constants/monitor')
 const cache = require('../../utils/cache')
@@ -51,11 +51,19 @@ function fetchFromTodayTotal() {
   const sql = `SELECT COUNT(DISTINCT \`from\`) as total FROM ${table} WHERE TO_DAYS(create_time) = TO_DAYS(NOW())`
   return mysql.mysql.raw(sql)
 }
+// 获取最旧时间
+function fetchStartTime() {
+  const sql = `SELECT create_time FROM ${table} ORDER BY create_time ASC LIMIT 1 OFFSET 0`
+  return mysql.mysql.raw(sql)
+}
 
 // 统计结果缓存一分钟
 module.exports = async (ctx) => {
   const oldStatistics = cache.get(cacheKey)
-  if (oldStatistics) return oldStatistics
+  if (oldStatistics) {
+    return (ctx.state.data = oldStatistics)
+  }
+  const [[{ create_time: start_time } = {}] = []] = await fetchStartTime()
   const res = await Promise.all([
     fetchJSErrorTotal(),
     fetchAssetErrorTotal(),
@@ -76,7 +84,10 @@ module.exports = async (ctx) => {
     'personEffetTodayTotal',
     'fromTodayTotal',
   ]
-  const data = {}
+  const data = {
+    start_time: moment(start_time).format('YYYY/MM/DD HH:mm:ss'),
+    end_time: moment().format('YYYY/MM/DD HH:mm:ss'),
+  }
   res.forEach(([[{ total } = {}] = []] = [], i) => {
     const key = keys[i]
     data[key] = total
