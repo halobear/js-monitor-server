8e02c0e609c69d36bb11eb5d4c2bd6ca3a91d322
luzhongkuan <luzhongk@126.com>
2020-08-21 15:57:19 +0800

    feat: 统计项目列表错误

--- server/controller/monitor/projects.js
+++ server/controller/monitor/projects.js
@@ -1,8 +1,18 @@
 const mysql = require('../../utils/mysql')
 const table = 'halo_errors'
 
-module.exports = async (ctx) => {
-  const projects = await mysql.mysql(table).distinct('pid')
+const cache = require('../../utils/cache')
+
+function fetchProjects() {
+  return mysql
+    .mysql(table)
+    .count('pid as error')
+    .select(['pid', 'create_time'])
+    .orderBy('create_time')
+    .groupBy('pid')
+}
+
+async function fetchTodayErrorsMap() {
   const errors = await mysql
     .mysql(table)
     .count('pid as total')
@@ -14,8 +24,20 @@ module.exports = async (ctx) => {
     obj[item.pid] = item.total || 0
     return obj
   }, {})
+  return errorsMap
+}
+
+module.exports = async (ctx) => {
+  const key = 'monitor_projects'
+  const oldCache = cache.get(key)
+  if (oldCache) {
+    return (ctx.state.data = oldCache)
+  }
+  const [projects, errorsMap] = await Promise.all([fetchProjects(), fetchTodayErrorsMap()])
   projects.forEach((item) => {
-    item.error = errorsMap[item.pid] || 0
+    item.todayError = errorsMap[item.pid] || 0
   })
+  // 缓存1分钟
+  cache.set(key, projects, 1000 * 60)
   ctx.state.data = projects
 }
