b3a32969171c38589a7cdb28d6d1602240cb7e56
luzhongkuan <luzhongk@126.com>
2020-08-23 14:32:01 +0800

    fix: 添加性能统计表

--- static/halo_monitor.sql
+++ static/halo_monitor.sql
@@ -1,24 +1,24 @@
 /*
- Navicat Premium Data Transfer
+ Navicat MySQL Data Transfer
 
- Source Server         : local_dev
+ Source Server         : 121.36.255.88
  Source Server Type    : MySQL
- Source Server Version : 50731
- Source Host           : localhost
- Source Database       : halo_monitor
+ Source Server Version : 50644
+ Source Host           : 121.36.255.88:3306
+ Source Schema         : halo_monitor
 
  Target Server Type    : MySQL
- Target Server Version : 50731
- File Encoding         : utf-8
+ Target Server Version : 50644
+ File Encoding         : 65001
 
- Date: 08/20/2020 16:08:45 PM
+ Date: 23/08/2020 14:31:23
 */
 
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `halo_errors`
+-- Table structure for halo_errors
 -- ----------------------------
 DROP TABLE IF EXISTS `halo_errors`;
 CREATE TABLE `halo_errors` (
@@ -32,10 +32,10 @@ CREATE TABLE `halo_errors` (
   `create_time` datetime NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=319 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
---  Table structure for `halo_manage`
+-- Table structure for halo_manage
 -- ----------------------------
 DROP TABLE IF EXISTS `halo_manage`;
 CREATE TABLE `halo_manage` (
@@ -48,5 +48,26 @@ CREATE TABLE `halo_manage` (
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for halo_performance
+-- ----------------------------
+DROP TABLE IF EXISTS `halo_performance`;
+CREATE TABLE `halo_performance` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `pid` varchar(20) CHARACTER SET utf8mb4 NOT NULL,
+  `uid` varchar(20) CHARACTER SET utf8mb4 NOT NULL,
+  `white_time` int(15) NOT NULL,
+  `load_time` int(15) NOT NULL,
+  `dom_use_time` int(15) NOT NULL,
+  `redirect_time` int(15) NOT NULL,
+  `response_time` int(15) NOT NULL,
+  `dns_query_time` int(15) NOT NULL,
+  `dns_cache_time` int(15) NOT NULL,
+  `tcp_time` int(15) NOT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
