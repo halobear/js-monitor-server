200c5459d0decd2c61aed10e858190275effde0c
luzhongkuan <luzhongk@126.com>
2020-08-24 08:17:43 +0800

    fix: 上报主动过滤字段

--- server/controller/monitor/performance.js
+++ server/controller/monitor/performance.js
@@ -3,10 +3,32 @@ const table = 'halo_performance'
 
 // 性能统计上报
 module.exports = async (ctx) => {
-  const query = ctx.request.query
-  if (!query.pid || !query.uid) {
+  const {
+    pid,
+    uid,
+    white_time,
+    load_time,
+    dom_use_time,
+    redirect_time,
+    response_time,
+    dns_query_time,
+    dns_cache_time,
+    tcp_time,
+  } = ctx.query
+  if (!pid || !uid) {
     return (ctx.state.body = '')
   }
-  ctx.state.data = await mysql.add(table, query)
-  console.log(ctx.state.data)
+  const data = {
+    pid,
+    uid,
+    white_time,
+    load_time,
+    dom_use_time,
+    redirect_time,
+    response_time,
+    dns_query_time,
+    dns_cache_time,
+    tcp_time,
+  }
+  ctx.state.data = await mysql.add(table, data)
 }
